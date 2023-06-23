af33fb168e4a094cdbfbe6ba6a1128c24ce25e06
dimzachar <zacharenakis@proton.me>
2023-06-23 21:44:51 +0000

    test

---
+++ quote_gen/tests/test_app.py
@@ -0,0 +1,24 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
+    rv = client.get('/health')
+    assert rv.status_code == 200
+    assert rv.data == b'healthy'
+
+def test_quote(client):
+    rv = client.get('/quote')
+    assert rv.status_code == 200
+
+def test_root(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b"Quote Gen Service" in rv.data
+
+
